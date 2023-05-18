1642d7665f0fd90fa09199502aa23541ae2fb789
Danyma0602 <dann_molar02@hotmail.com>
2023-05-18 14:56:21 -0600

    add migration hotels

--- database/migrations/2023_05_18_193509_create_hotels_table.php
+++ database/migrations/2023_05_18_193509_create_hotels_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
+            $table->string('Name', 100);
+            $table->enum('Rating', [1, 2, 3, 4, 5]);
+            $table->string('City', 40);
+            $table->decimal('Price_per_night', 8, 2);
+            $table->string('Image');
             $table->timestamps();
         });
     }
