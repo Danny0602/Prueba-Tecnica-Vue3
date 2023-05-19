dd1af2eb6f36e9ef989eb8033dd26355af4e94a7
Danyma0602 <dann_molar02@hotmail.com>
2023-05-19 02:16:08 -0600

    documentando metodos del controaldor de hoteles

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -11,7 +11,11 @@ class HotelController extends Controller
 
     public function index()
     {
-
+        /* This code is retrieving all the hotel records from the database using the `all()` method of
+       the `hotel` model and storing them in the `` variable. Then, it is rendering the
+       "index" view for the hotels using the Inertia.js library and passing the `` variable
+       to the view as a prop called "hoteles". This allows the Vue.js component in the view to
+       access the hotel data and display it in a table or list format. */
         $hoteles = hotel::all();
         return Inertia::render('Hotels/index', ['hoteles' => $hoteles]);
     }
@@ -19,12 +23,17 @@ class HotelController extends Controller
 
     public function create()
     {
+        /* `return Inertia::render('Hotels/create');` is rendering the "create" view for the hotels. It
+        is using the Inertia.js library to render a Vue.js component on the client-side. The view is
+        passed to the component which contains a form for creating a new hotel record. The user can
+        fill in the form fields such as the hotel name, city, rating, price per night, and image,
+        and submit the form to create a new hotel record in the database. */
         return Inertia::render('Hotels/create');
     }
 
     public function store(Request $request)
     {
-
+        /* This code is handling the creation of a new hotel record in the database. */
         $request->validate([
             'Name' => 'required',
             'Rating' => 'required',
@@ -48,7 +57,11 @@ class HotelController extends Controller
 
     public function show(hotel $hotel)
     {
-
+        /* This code is rendering the "show" view for a specific hotel. It is using the Inertia.js
+        library to render a Vue.js component on the client-side. The view is passed a variable
+        called "hotel" which contains the data for the specific hotel being shown. This data is used
+        to populate the fields in the view, such as the hotel name, city, rating, price per night,
+        and image. */
         return Inertia::render('Hotels/show', [
             'hotel' => $hotel
         ]);
@@ -57,6 +70,10 @@ class HotelController extends Controller
 
     public function update(Request $request)
     {
+        /* This code is updating an existing hotel record in the database. It first finds the hotel
+        record based on the ID passed in the request, then updates the record with the new values
+        for the Name, City, Rating, Price_per_night, and Image fields. Finally, it saves the updated
+        record to the database and redirects the user back to the index page for hotels. */
         $hotel = hotel::find($request->id);
         $hotel->Name = $request->Name;
         $hotel->City = $request->City;
@@ -70,11 +87,12 @@ class HotelController extends Controller
 
 
 
-    public function destroy(hotel $hotel){
-
+    public function destroy(hotel $hotel)
+    {
+        /* This code is deleting an existing hotel record from the database. It first finds the hotel
+       record based on the ID passed in the request, then deletes the record from the database.
+       Finally, it redirects the user back to the index page for hotels. */
         $hotel->delete();
-
         return redirect()->route('hoteles.index');
-
     }
 }
